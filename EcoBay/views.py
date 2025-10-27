988bad4a95526b8122213b9363902241b46151e1
Sanya Hussain <sanz_133@hotmail.com>
2025-10-27 15:54:49 +0000

    added boilerplate views

--- EcoBay/views.py
+++ EcoBay/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
 
-def index(request):
+def home(request):
     return render(request, 'index.html')
+
+
+def register(request):
+    pass
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
+
+def categories(request):
+    pass
+
+def list_items(request):
+    pass
+
+def add_item(request):
+    pass
+
+def get_item(request):
+    pass
