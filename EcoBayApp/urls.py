bd3702e1b455360967cd2b7f5c212eae88e0fe20
Sanya Hussain <sanz_133@hotmail.com>
2025-11-05 17:26:22 +0000

    updated url paths with login and register

--- EcoBayApp/urls.py
+++ EcoBayApp/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('/register', views.register, name='register'),
     path('/login', views.login, name='login'),
+    path('/items', views.login, name='login'),
+    path('/skills', views.login, name='login'),
 ]
