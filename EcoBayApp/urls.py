f9868e4dff3d8982cf6c514d70cb48da271635fb
Sanya Hussain <sanz_133@hotmail.com>
2025-11-14 10:35:11 +0000

    updated url paths

--- EcoBayApp/urls.py
+++ EcoBayApp/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('/login', views.login, name='login'),
     path('/items', views.list_items, name='list_items'),
     path('/skills', views.list_skills, name='list_skills'),
+    path('/skills-request', views.request_skill, name='request_skill'),
+    path('/add-items', views.add_item, name='add_item'),
+    path('/add-skill', views.add_skill, name='add_skill'),
 ]
