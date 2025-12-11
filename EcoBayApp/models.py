e683383cd40271aa9073bf9eb50fa27db691b901
Sanya Hussain <sanz_133@hotmail.com>
2025-12-11 18:08:53 +0000

    updated classes with foreign key relation

--- EcoBayApp/models.py
+++ EcoBayApp/models.py
@@ -13,6 +13,8 @@ class Item(models.Model):
     description = models.CharField(max_length=200)
     amount = models.DecimalField(max_digits=5, decimal_places=2)
     is_active = models.BooleanField(default=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 class Skill(models.Model):
     id = models.CharField(max_length=40, unique=True, primary_key=True)
@@ -21,11 +23,13 @@ class Skill(models.Model):
     description = models.CharField(max_length=200)
     amount = models.DecimalField(max_digits=5, decimal_places=2)
     is_active = models.BooleanField(default=True)
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Offer(models.Model):
     id = models.CharField(max_length=40, unique=True, primary_key=True)
     name = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
     
