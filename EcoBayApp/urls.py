8f5ac58ea3feb2ac9ec90d3af9e4e06a6686499c
Sanya Hussain <sanz_133@hotmail.com>
2025-12-28 14:50:05 +0000

    updated url routes skill/item to take id parameter

--- EcoBayApp/urls.py
+++ EcoBayApp/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('add-item/', views.add_item, name='add_item'),
     path('add-skill/', views.add_skill, name='add_skill'),
     path('search/', views.search, name='search'),
+    path('item/<int:id>/', views.get_item, name='get_item'),
+    path('skill/<int:id>/', views.get_skill, name='get_skill'),
     path('', views.home, name='home'),
 ]
