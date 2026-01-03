5da81ef8ac04726aac2fea1d3377a156616ba6ed
Sanya Hussain <sanz_133@hotmail.com>
2026-01-03 18:01:48 +0000

    wrote category test

--- EcoBayApp/tests/test_models.py
+++ EcoBayApp/tests/test_models.py
@@ -5,11 +5,21 @@ from model_bakery import baker
 class UserTestModel(TestCase):
     def setUp(self):
         self.user = baker.make(User)
-        print(self.user)
     
     def test_user_created(self):
         self.assertIsNotNone(self.user.pk)
     
     def test_user_in_db(self):
         users = User.objects.all()
-        print(users)
+
+
+class CategoryTestModel(TestCase):
+    def setUp(self):
+        self.category = baker.make(Category)
+    
+    def test_category_created(self):
+        self.assertIsNotNone(self.category.pk)
+    
+    def test_category_in_db(self):
+        categories = Category.objects.all()
+        print(categories)
