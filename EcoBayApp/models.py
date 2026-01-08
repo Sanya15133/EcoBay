d7dd439e5d78d1442036a8a5978810e770ca67bf
Sanya Hussain <sanz_133@hotmail.com>
2026-01-08 10:27:33 +0000

    added basic fixtures json data for categories

--- EcoBayApp/models.py
+++ EcoBayApp/models.py
@@ -6,11 +6,11 @@ class User(AbstractUser):
     pass
 
 class Category(models.Model):
-    id = models.CharField(max_length=40, unique=True, primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
     
 class Item(models.Model):
-    id = models.CharField(max_length=40, unique=True, primary_key=True)
+    id = models.AutoField(primary_key=True)
     image_url = models.URLField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=20)
@@ -21,7 +21,7 @@ class Item(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 class Skill(models.Model):
-    id = models.CharField(max_length=40, unique=True, primary_key=True)
+    id = models.AutoField(primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=200)
@@ -31,7 +31,7 @@ class Skill(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 class Offer(models.Model):
-    id = models.CharField(max_length=40, unique=True, primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
