ad5cba4dfba2640d2e7509dacdd767a80f352a8e
Sanya Hussain <sanz_133@hotmail.com>
2026-01-25 10:45:20 +0000

    need to fix created_at as not giving timestamp

--- EcoBayApp/models.py
+++ EcoBayApp/models.py
@@ -12,7 +12,6 @@ class Category(models.Model):
 class Item(models.Model):
     id = models.AutoField(primary_key=True)
     image_url = models.URLField(max_length=500)
-    created_at = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=200)
     amount = models.DecimalField(max_digits=5, decimal_places=2)
