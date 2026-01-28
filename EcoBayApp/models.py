530f2bf8b654b5b865b68e093b8759024d9868fc
Sanya Hussain <sanz_133@hotmail.com>
2026-01-28 11:33:13 +0000

    fixed created_at

--- EcoBayApp/models.py
+++ EcoBayApp/models.py
@@ -12,6 +12,7 @@ class Category(models.Model):
 class Item(models.Model):
     id = models.AutoField(primary_key=True)
     image_url = models.URLField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=200)
     amount = models.DecimalField(max_digits=5, decimal_places=2)
@@ -21,10 +22,10 @@ class Item(models.Model):
 
 class Skill(models.Model):
     id = models.AutoField(primary_key=True)
-    created_at = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=200)
     amount = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
