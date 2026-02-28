46ca67139aa8858e6967e1a5a1bdeedb321d1dff
Sanya Hussain <sanz_133@hotmail.com>
2026-02-28 20:14:39 +0000

    started testing views

--- EcoBayApp/tests/test_views.py
+++ EcoBayApp/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 
 class MyViewsTests(TestCase):
 
@@ -6,6 +7,12 @@ class MyViewsTests(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
     
+    def test_template_used(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "index.html")
+    
     def test_search_bar(self):
-        response = self.client.get('/search/', {'query': 'cat'})
+        response = self.client.get('/search/', {'query': 'laptop'})
         self.assertEqual(response.status_code, 200)
+    
+
