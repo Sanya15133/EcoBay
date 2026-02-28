1b0b4ad82c40bf7e1f781b78b2fc34e9f9b8b3ab
Sanya Hussain <sanz_133@hotmail.com>
2026-02-28 19:34:33 +0000

    started testing views

--- EcoBayApp/tests/test_views.py
+++ EcoBayApp/tests/test_views.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
 
-class MyViewTests(TestCase):
+class MyViewsTests(TestCase):
 
     def test_homepage(self):
         response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_search_bar(self):
+        response = self.client.get('/search/', {'query': 'cat'})
         self.assertEqual(response.status_code, 200)
